263b6f324dd86caa46e8f1328312f72d0cfa798f
pasantedesarrollo1CD <pasantedesarrollo1@wanqara.com>
2025-04-08 18:48:47 -0500

    indexClient

--- src/pages/wizards/cliente/indexCliente.vue
+++ src/pages/wizards/cliente/indexCliente.vue
@@ -7,18 +7,37 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import WizardBase from "@/components/wizard/wizard-base.vue";
-import personalData from "@/pages/wizards/common/personalData.vue";
-import { getCommonSubStepsConfig } from "@/utils/wizard-imports";
+import { ref } from "vue"
+import WizardBase from "@/components/wizard/wizard-base.vue"
+import personalData from "@/pages/wizards/common/personalData.vue"
+import { getWizardComponents } from "@/utils/wizard-imports"
 
 // Definimos los componentes para cada paso principal
 const stepComponents = {
-  'personal-info': personalData,
+  "personal-info": personalData,
   // Otros pasos principales que no tienen sub-pasos
-};
+}
 
-// Obtenemos la configuración común de sub-pasos
-const subStepsConfig = ref(getCommonSubStepsConfig());
+// Obtenemos los componentes del wizard
+const components = getWizardComponents()
+
+// Creamos una configuración personalizada para el cliente que excluye planes y método de pago
+const subStepsConfig = ref({
+  "create-company": [
+    { title: "Información Básica", component: components.companyInfo },
+    { title: "Email del ticket", component: components.companyTicketEmail },
+    { title: "Establecer un Logo", component: components.configLogo },
+    { title: "Dominio", component: components.companyDomain },
+  ],
+  "config-company": [
+    // Se omiten los sub-setups 1 (planes) y 2 (método de pago)
+    { title: "Configuracion de RIMPE", component: components.configRipe },
+    { title: "Preguntas simples", component: components.simpleQuestion },
+    { title: "Datos de la Sucursal", component: components.companySucursal },
+    { title: "Preguntas simples Sucursal", component: components.simpleQuestionSucursal },
+    { title: "Creacion de Punto de Venta", component: components.companyPOS },
+  ],
+})
+
 </script>
 
