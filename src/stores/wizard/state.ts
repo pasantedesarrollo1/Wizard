7388a03d3bb125d1bbe3e9828b8ad9c016f4a201
pasantedesarrollo1CD <pasantedesarrollo1@wanqara.com>
2025-04-12 21:26:12 -0500

    actualizacion 3

--- src/stores/wizard/state.ts
+++ src/stores/wizard/state.ts
@@ -10,7 +10,7 @@ export const initialState = (): WizardInterface => ({
   formData: {
     personalInfo: {
       identification: {
-        type: "ruc",
+        type: "",
         number: "",
       },
       name: {
